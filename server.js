7af7167bfbf72d31fc41ad05157b4b980205da79
imane imziline <imaneimzline2@gmail.com>
2026-04-13 09:25:31 +0100

    ajpoté route de paiment

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import userRoute from "./routes/authRouter.js";
 import forniseureRoute from "./routes/forniseureRout.js";
-import factureRouter from "./routes/factureRoute.js"
+import factureRouter from "./routes/factureRoute.js";
+import paimentreRoute from "./routes/paimentRout.js"
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/auth", userRoute);
 app.use("/api/suppliers", forniseureRoute);
 app.use("/api/invoices", factureRouter);
+app.use("/api/paiment", paimentreRoute);
 
 app.listen(3000, () => {
     console.log("Server running");
